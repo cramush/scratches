1461c3d4cc95479f5d6a6f8367e7289597bc2cc2
cramush <cramush@gmail.com>
2021-09-01 15:39:15 +0300

    add easy_7_palindrome_number.py

---
+++ easy_7_palindrome_number.py
@@ -0,0 +1,66 @@
+# Given an integer x, return true if x is palindrome integer.
+# An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+#
+# Example 1:
+# Input: x = 121
+# Output: true
+#
+# Example 2:
+# Input: x = -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+#
+# Example 3:
+# Input: x = 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+#
+# Example 4:
+# Input: x = -101
+# Output: false
+#
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        if x < 0:
+            return False
+
+        left = []
+        right = []
+        str_x = str(x)
+        list_x = list(str_x)
+
+        if len(str_x) % 2 == 0:
+            half_list = len(str_x) // 2
+
+
+            for i in range(half_list):
+                left.append(list_x[i])
+                right.append(list_x.pop(-1))
+
+            if left == right:
+                return True
+
+            else:
+                return False
+
+        else:
+            half_list = (len(str_x) - 1) // 2
+
+            for i in range(half_list):
+                left.append(list_x[i])
+                right.append(list_x.pop(-1))
+
+            if left == right:
+                return True
+
+            else:
+                return False
+
+
+sol = Solution()
+print(sol.isPalindrome(x = -4113))
